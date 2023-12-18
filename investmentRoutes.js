54fae2350c2823ee0856f674cdf2baedb34777e9
arihant-101 <bharadwaj.arihant101@gmail.com>
2023-12-18 16:03:05 +0530

    Update InvestmentRoutes
    updated investment apis

--- investmentRoutes.js
+++ investmentRoutes.js
@@ -1,69 +1,51 @@
 const express = require('express');
+const client = require('./pgdatabase');
+
 const router = express.Router();
-const db = require('./pgdatabase');
-const client = require('./pgdatabase'); // Import your database connection
 
-
-
+// Function to get user investments without authentication
 async function getUserInvestments(phoneNo) {
-    try {
-      const result = await client.query(
-        'SELECT * FROM public."investments" WHERE "PhoneNo" = $1',
-        [phoneNo]
-      );
-  
-      return result.rows;
-    } catch (error) {
-      throw error;
-    }
-  }
-  
-  async function addInvestment(planId, phoneNo, amount, durationLeft) {
-    try {
-      // Generate investedAt as today's date
-      const investedAt = new Date();
-  
-      // Calculate endDate by adding durationLeft months to investedAt
-      const endDate = new Date(investedAt);
-      endDate.setMonth(endDate.getMonth() + durationLeft);
-  
-      const result = await client.query(
-        'INSERT INTO public."investments" ("PlanId", "PhoneNo", "Amount", "DurationLeft", "CurrentAmount", "InvestedAt", "EndDate") VALUES ($1, $2, $3, $4, $3, $5, $6) RETURNING *',
-        [planId, phoneNo, amount, durationLeft, investedAt, endDate]
-      );
-  
-      // Refresh the portfolio materialized view  
-      await client.query('SELECT refresh_portfolio()');
-  
-      return result.rows[0];
-    } catch (error) {
-      throw error;
-    }
-  }
-  
-  router.get('/investments/:phoneNo', async (req, res) => {
-    const { phoneNo } = req.params;
-  
-    try {
-      const investments = await getUserInvestments(phoneNo);
-      res.json(investments);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-  
-  router.post('/investments', async (req, res) => {
-    const { planId, phoneNo, amount, durationLeft } = req.body;
-  
-    try {
-      const investment = await addInvestment(planId, phoneNo, amount, durationLeft);
-      res.json(investment);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-  // ... (other routes and code)
-  
+  try {
+    const result = await client.query(
+      'SELECT * FROM public."investments" WHERE "PhoneNo" = $1',
+      [phoneNo]
+    );
 
+    return result.rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Function to add an investment without authentication
+async function addInvestment(planId, phoneNo, amount, monthsLeft, DurationLeft, CurrentAmount, endDate, nextDueDate, status, frequency) {
+  try {
+    const investedAt = new Date().toISOString().split('T')[0]; // Simplified date format
+
+    const result = await client.query(
+      'INSERT INTO public."investments" ("PlanId", "PhoneNo", "Amount", "InvestedAt", "DurationLeft", "CurrentAmount", "EndDate", "NextDueDate", "MonthsLeft", "Status", "Frequency") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *',
+      [planId, phoneNo, amount, investedAt, DurationLeft, amount, endDate, nextDueDate, monthsLeft, status, frequency]
+      // Set CurrentAmount to amount
+    );
+
+    return result.rows[0];
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Example API endpoint for creating an investment without authentication
+router.post('/investments', async (req, res) => {
+  const { planId, phoneNo, amount, monthsLeft, DurationLeft, CurrentAmount, endDate, nextDueDate, status, frequency } = req.body;
+
+  try {
+    const investment = await addInvestment(planId, phoneNo, amount, monthsLeft, DurationLeft, amount, endDate, nextDueDate, status, frequency);
+    res.json(investment);
+  } catch (error) {
+    res.status(500).json({ error: 'Error creating investment', details: error.message });
+  }
+});
+
+// Add more endpoints as needed
 
 module.exports = router;
